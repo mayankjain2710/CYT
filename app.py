88653904f316123185be74acc6e4ebce809c2305
Mayank Jain <158475523+mayankjain2710@users.noreply.github.com>
2024-08-14 23:01:38 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,request, render_template # type: ignore
+import configparser
+import numpy as np # type: ignore
+import pickle
+import sklearn # type: ignore
+print(sklearn.__version__)
+#loading models
+dtr = pickle.load(open('C:/Users/Hp/Agrosage Platform/H2S CYT Tool/dtr.pkl','rb'))
+preprocessor = pickle.load(open('C:/Users/Hp/Agrosage Platform/H2S CYT Tool/preprocesser.pkl', 'rb'))
+
+
+#flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route("/predict",methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        Year = request.form['Year']
+        average_rain_fall_mm_per_year = request.form['average_rain_fall_mm_per_year']
+        pesticides_tonnes = request.form['pesticides_tonnes']
+        avg_temp = request.form['avg_temp']
+        Area = request.form['Area']
+        Item  = request.form['Item']
+
+        features = np.array([[Year,average_rain_fall_mm_per_year,pesticides_tonnes,avg_temp,Area,Item]],dtype=object)
+        transformed_features = preprocessor.transform(features)
+        prediction = dtr.predict(transformed_features).reshape(1,-1)
+
+        return render_template('index.html',prediction = prediction[0][0])
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
